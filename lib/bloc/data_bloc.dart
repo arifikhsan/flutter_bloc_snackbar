a247b535b4fa1fa0f7f95128dccca181706b6bac
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-05-18 00:01:22 +0700

    done

---
+++ lib/bloc/data_bloc.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'data_event.dart';
+part 'data_state.dart';
+
+class DataBloc extends Bloc<DataEvent, DataState> {
+  @override
+  DataState get initialState => Initial();
+
+  @override
+  Stream<DataState> mapEventToState(
+    DataEvent event,
+  ) async* {
+    if (event is FetchData) {
+      yield Loading();
+      await Future.delayed(Duration(seconds: 2));
+      yield Success();
+    }
+  }
+}
---
+++ lib/bloc/data_state.dart
@@ -0,0 +1,10 @@
+part of 'data_bloc.dart';
+
+@immutable
+abstract class DataState {}
+
+class Initial extends DataState {}
+
+class Loading extends DataState {}
+
+class Success extends DataState {}
